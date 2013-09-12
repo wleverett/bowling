0bb36390022f4ac59a86d71896680a51f454d172
MakerSquare Student <students@themakersquare.com>
2013-09-12 09:56:44 -0500

    first commit

---
+++ spec/bowling_game_spec.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'rspec'
+require_relative '../bowling_game'
+
+=begin
+describe Bowling::xxx do
+
+  describe '.initialize' do
+    it 'xxx' do
+    end
+  end
+end
+=end
